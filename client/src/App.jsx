fc6a64d74fb84c87cce6d4015fcfc5498ef40425
Cathy Dao <cathydao95@gmail.com>
2023-10-02 14:57:20 -0700

    add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,47 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import SinglePost from "./pages/SinglePost.jsx";
+import CreatePost from "./pages/CreatePost.jsx";
+import DashboardLayout from "./components/DashboardLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "posts/:id",
+        element: <SinglePost />,
+      },
+      {
+        path: "create",
+        element: <CreatePost />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
