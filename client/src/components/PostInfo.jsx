75400f5abb3aa3743b554360f6a4df1c8b97a2f1
Cathy Dao <cathydao95@gmail.com>
2023-10-03 13:14:21 -0700

    slice content to show only 250 chars

--- client/src/components/PostInfo.jsx
+++ client/src/components/PostInfo.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getText } from "../utils";
+
 const PostInfo = ({ post }) => {
-  const { id, title, image, content, author, created_at } = post;
+  const { id, title, img, content, author, created_at } = post;
+  console.log(post);
   return (
     <div className="post">
       <div className="imgContainer">
-        <img className="img" src={image} alt="lol" />
+        <img className="img" src={`../../public/uploads/${img}`} alt="lol" />
       </div>
       <div className="postContent">
         <Link to={`posts/${id}`}>
-          <h2>{title}</h2>
-          <p>{content}</p>
-          <button>Read More</button>
+          <h2 className="title">{title}</h2>
+          <p className="content">{`${getText(content.slice(0, 250))}...`}</p>
+          <button className="readMore">Read More</button>
         </Link>
       </div>
     </div>
