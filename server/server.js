51c9eb81034123ab0365fd2d971d9035f7686448
Cathy Dao <cathydao95@gmail.com>
2023-10-02 22:42:52 -0700

    add postRoute and multer

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import path from "path";
 import "dotenv/config";
 import db from "./db/db-connection.js";
+import postRoute from "./routes/posts.js";
+import multer from "multer";
 
 const app = express();
 const PORT = 8080;
@@ -13,91 +15,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//
-app.get("/", (req, res) => {
-  res.json("Hello ");
+const upload = multer({ dest: "uploads/" });
+app.post("/upload", upload.single("file"), (req, res, next) => {
+  res.status(200).json("Image has been uploaded");
 });
 
-app.get("/api/v1/posts", async (req, res) => {
-  try {
-    const { rows: posts } = await db.query(
-      "SELECT * FROM posts ORDER BY created_at DESC"
-    );
-    res.status(200).json({
-      status: "success",
-      results: posts.length,
-      data: { posts },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error });
-  }
-});
-
-app.get("/api/v1/posts/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const { rows: post } = await db.query("SELECT * FROM posts WHERE id=$1", [
-      id,
-    ]);
-    res.status(200).json({
-      status: "success",
-      data: { post },
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// CREATE CONTACT
-app.post("/api/v1/posts", async (req, res) => {
-  try {
-    const { title, content, author } = req.body;
-
-    const { rows: newPost } = await db.query(
-      "INSERT INTO posts (title, content, author) VALUES ($1, $2, $3) RETURNING *",
-      [title, content, author]
-    );
-    res.status(200).json({
-      status: "success",
-      data: { newPost },
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error });
-  }
-});
-
-app.put("/api/v1/posts/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, content, author } = req.body;
-
-    const { rows: updatedPost } = await db.query(
-      "UPDATE posts SET (title, content, author) = ($1, $2, $3) WHERE id = $4 RETURNING *",
-      [title, content, author, id]
-    );
-    res.status(200).json({
-      status: "success",
-      data: { updatedPost },
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.delete("/api/v1/posts/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedPost = await db.query("DELETE FROM posts WHERE id = $1", [id]);
-    res.status(200).json({
-      status: "success",
-    });
-  } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+// routes
+app.use("/api/v1/posts", postRoute);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
