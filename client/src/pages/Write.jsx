297b0116a44bd6d19274a073504798ce3de81af2
Cathy Dao <cathydao95@gmail.com>
2023-10-03 13:06:47 -0700

    add image upload functionality

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Write = () => {
   const { state } = useLocation();
+
+  const navigate = useNavigate();
+
   const [title, setTitle] = useState(state?.title || "");
   const [value, setValue] = useState(state?.content || "");
   const [file, setFile] = useState(null);
 
-  // const uploadImage = async () => {
-  //   try {
-  //     const formData = new FormData();
-  //     formData.append("file", file);
-  //     const res = await fetch("/upload", formData);
-  //     return res.data;
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  console.log("state", state);
+  console.log(file);
+  const uploadImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log(formData);
+      const response = await fetch("http://localhost:8080/api/v1/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-  const handleSubmit = async () => {
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const imgUrl = await uploadImage();
+    console.log(imgUrl);
     try {
       let response;
       state
@@ -31,7 +47,11 @@ const Write = () => {
               headers: {
                 "Content-type": "application/JSON",
               },
-              body: JSON.stringify({ title, value, file }),
+              body: JSON.stringify({
+                title,
+                content: value,
+                img: file ? imgUrl : "",
+              }),
             }
           ))
         : (response = await fetch("http://localhost:8080/api/v1/posts", {
@@ -39,13 +59,18 @@ const Write = () => {
             headers: {
               "Content-type": "application/JSON",
             },
-            body: JSON.stringify(title, value, file),
+            body: JSON.stringify({
+              title,
+              content: value,
+              img: file ? imgUrl : "",
+            }),
           }));
 
       if (response.ok) {
         const {
           data: { post },
         } = await response.json();
+        navigate("/");
       } else {
         throw new Error("Network response was not ok");
       }
@@ -53,30 +78,26 @@ const Write = () => {
       console.error(error);
     }
   };
-  const handleInput = (e) => {
-    setValue((prevValues) => {
-      return { ...prevValues, [e.target.name]: e.target.value };
-    });
-  };
 
   console.log(value);
   return (
     <div className="createPost">
       <form className="form">
+        <label htmlFor="titleInput">Post Title:</label>
         <input
           name="title"
-          className="titleInput"
+          id="titleInput"
           type="text"
           placeholder="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <div>
+        <div className="fileUpload">
           <label htmlFor="file">Upload Image</label>
           <input
             type="file"
             id="file"
-            // onChange={(e) => setFile(e.target.files[0])}
+            onChange={(e) => setFile(e.target.files[0])}
           />
         </div>
         <div className="textareaContainer">
@@ -88,6 +109,13 @@ const Write = () => {
             onChange={setValue}
           />
         </div>
+        <button
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          {state ? "Update Post" : "Create Post"}
+        </button>
       </form>
     </div>
   );
